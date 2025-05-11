bfc691158cd24d63e7640960a83c1fa0afa1e51a
Matthew Phillip Cooper <coopertronics@gmail.com>
2025-05-11 18:36:31 +0100

    Old sudo pacman commented out in test script.

--- test-deps.sh
+++ test-deps.sh
@@ -2,4 +2,4 @@
 
 #   Test that the dependencies for Rescuezilla can be found on Arch
 
-sudo pacman -Sy 
+# sudo pacman -Syu smbclient fsarchiver partclone pv pigz nmap os-prober gvfs cifs-utils sshfs sshpass nfs-common file xdg-utils libnotify-bin lshw pciutils yad hdparm pbzip2 lzop pixz plzip lrzip zstd fatresize archivemount nbd-server nbd-client python3-babel clonezilla lvm2 binutils smartmontools qemu-utils efibootmgr bsdmainutils
